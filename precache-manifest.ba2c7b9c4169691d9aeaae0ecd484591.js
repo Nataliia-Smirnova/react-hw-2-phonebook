eb2fb30601fd8b96176ef4d8bb5098760f63c32a
Nataliia <n3v3st4@gmail.com>
2020-09-07 23:22:04 +0300

    Updates

---
+++ precache-manifest.ba2c7b9c4169691d9aeaae0ecd484591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e530af39d2fe97582af8041d3f1ae46",
+    "url": "/react-hw-2-phonebook/index.html"
+  },
+  {
+    "revision": "94b5cfeb2ddcb8e64645",
+    "url": "/react-hw-2-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "94b5cfeb2ddcb8e64645",
+    "url": "/react-hw-2-phonebook/static/js/2.1d56abf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-2-phonebook/static/js/2.1d56abf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3c6a773cb8e6206da6",
+    "url": "/react-hw-2-phonebook/static/js/main.64d30c85.chunk.js"
+  },
+  {
+    "revision": "0b7f2b3b25b3218d2608",
+    "url": "/react-hw-2-phonebook/static/js/runtime-main.59106f6d.js"
+  }
+]);
